ea65070aadfce5ef9f6230b1e7d29303b63e9faa
Washingto Pires <4washingtonbr@gmail.com>
2020-04-17 02:37:20 -0300

    Update Storybook wrapper

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import 'normalize.css';
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
+import { Wrapper } from '../src/components/app/app-styles';
 
 const StorybookWrapper = (storyFn) => (
   <MemoryRouter>
-    {storyFn()}
+    <Wrapper>
+      {storyFn()}
+    </Wrapper>
   </MemoryRouter>
 );
 
