53c968f26f4bbc67e154609620ecc195ca3055d7
Washingto Pires <4washingtonbr@gmail.com>
2020-04-17 01:36:46 -0300

    Add DataFilter stories

---
+++ src/components/data-filter/data-filter-view.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DataFilterView from './data-filter-view';
+
+export default {
+  title: 'Components/Data Filter',
+};
+
+export const standard = () => <DataFilterView />;
