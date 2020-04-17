fd20418286948809aca1d75475ff676041f16179
Washingto Pires <4washingtonbr@gmail.com>
2020-04-17 02:22:08 -0300

    Add Open Sans font

---
+++ src/components/app/app-styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+`;
--- src/components/app/app-view.js
+++ src/components/app/app-view.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'normalize.css';
 import Routes from '../routes';
+import { Wrapper } from './app-styles';
 
 export default function AppView() {
   return (
-    <Routes />
+    <Wrapper>
+      <Routes />
+    </Wrapper>
   );
 }
