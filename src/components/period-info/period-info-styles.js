92c6a34e88531292f1ae3e99f750451e07a4cb3a
Washingto Pires <4washingtonbr@gmail.com>
2020-04-18 14:31:53 -0300

    Fix PeriodInfo styles

--- src/components/period-info/period-info-styles.js
+++ src/components/period-info/period-info-styles.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
+import IconCalendar from '../../images/icon-calendar.svg';
 
 export const Wrapper = styled.div`
-  width: 100%;
+  width: calc(100% - 70px);
   height: 54px;
-  background-color: #F6F7F9;
-  text-align: center;
   line-height: 54px;
+  margin-bottom: 30px;
+  background-color: #F6F7F9;
+  background-image: url(${IconCalendar});
+  background-repeat: no-repeat;
+  background-position: 40px center;
+  background-size: 18px;
+  padding-left: 70px;
+  color: #577082;
 `;
