bb51960377ffa81a6b79f0223ec2ca13db443647
Washingto Pires <4washingtonbr@gmail.com>
2020-04-18 23:34:33 -0300

    Add AppContainer component

---
+++ src/components/app/app-container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
+import AppView from './app-view';
+
+export default function AppContainer() {
+  return (
+    <Provider store={store}>
+      <AppView />
+    </Provider>
+  );
+}
