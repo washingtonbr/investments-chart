bf24d007b6aa5d09e114780be7cff53d7b61eacc
Washingto Pires <4washingtonbr@gmail.com>
2020-04-19 04:48:19 -0300

    Add getInvestmentsFiltered Redux selector

--- src/redux/investments/selectors.js
+++ src/redux/investments/selectors.js
@@ -1,8 +1,24 @@
+import { date } from '../../utils/date';
+
 export const getInvestmentsState = (state) => state.investments;
 
 export const getInvestments = (state) => getInvestmentsState(state).items;
 
 export const getPeriodSelected = (state) => getInvestmentsState(state).periodSelected;
+
+export const getInvestmentsFiltered = (state) => {
+  const { startDate, endDate } = getPeriodSelected(state);
+  const investments = getInvestments(state);
+  if (!startDate && !endDate)
+    return investments;
+
+  return investments
+    .filter((investment) => {
+      const [timestamp] = investment;
+      return date(timestamp).isSameOrAfter(startDate) && 
+        date(timestamp).isSameOrBefore(endDate);
+    });
+};
 
 export const isFetching = (state) => getInvestmentsState(state).isFetching;
 
