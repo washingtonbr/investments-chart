6cf4e3fe7454d605cafb335ba18e92475712854d
Washingto Pires <4washingtonbr@gmail.com>
2020-04-19 14:35:09 -0300

    Redux config on Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,25 @@
-import React from 'react';
 import 'normalize.css';
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import { Wrapper } from '../src/components/app/app-styles';
+import { mockInitialState } from '../src/redux/store.mock';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const reduxStore = mockStore(mockInitialState);
 
 const StorybookWrapper = (storyFn) => (
-  <MemoryRouter>
-    <Wrapper>
-      {storyFn()}
-    </Wrapper>
-  </MemoryRouter>
+  <Provider store={reduxStore}>
+    <MemoryRouter>
+      <Wrapper>
+        {storyFn()}
+      </Wrapper>
+    </MemoryRouter>
+  </Provider>
 );
 
 addDecorator(StorybookWrapper);
---
+++ src/redux/store.mock.js
@@ -0,0 +1,12 @@
+export const mockInitialState = {
+  investments: {
+    isFetching: false,
+    error: {},
+    periodSelected: {
+      label: 'Desde o início',
+      startDate: null,
+      endDate: null,
+    },
+    items: [],
+  },
+};
