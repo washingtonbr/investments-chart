c3c7d91db0094b5aa36899a39df2c06889a48301
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 17:46:30 -0300

    Add and configure Redux Persist

--- src/components/app/app-view.js
+++ src/components/app/app-view.js
@@ -1,16 +1,19 @@
 import 'normalize.css';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Wrapper } from './app-view.styles';
 import Routes from '../routes';
-import store from '../../redux/store';
+import { store, persistor } from '../../redux/store';
 
 export default function AppView() {
   return (
     <Provider store={store}>
-      <Wrapper>
-        <Routes />
-      </Wrapper>
+      <PersistGate persistor={persistor}>
+        <Wrapper>
+          <Routes />
+        </Wrapper>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,20 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default createStore(
-  rootReducer,
+export const store = createStore(
+  persistedReducer,
+  {},
   composeEnhancer(applyMiddleware(thunk)),
 );
+
+export const persistor = persistStore(store);
