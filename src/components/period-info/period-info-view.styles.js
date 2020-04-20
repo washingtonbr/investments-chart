bc8a4f8854cbda84a0ebe7fc8eaca347c258988a
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 20:17:06 -0300

    Layout adjustments

--- src/components/period-info/period-info-view.styles.js
+++ src/components/period-info/period-info-view.styles.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 import IconCalendar from '../../images/icon-calendar.svg';
 
 export const Wrapper = styled.div`
-  width: calc(100% - 70px);
-  height: 54px;
-  line-height: 54px;
+  box-sizing: border-box;
+  width: 100%;
   margin-bottom: 30px;
   background-color: #F6F7F9;
   background-image: url(${IconCalendar});
   background-repeat: no-repeat;
   background-position: 40px center;
   background-size: 18px;
-  padding-left: 70px;
+  padding: 19px 30px 19px 70px;
+  line-height: 18px;
   color: #577082;
 `;
--- src/pages/investments/investments-view.styles.js
+++ src/pages/investments/investments-view.styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 800px;
+  width: calc(100% - 30px);
+  max-width: 800px;
   margin: 50px auto;
 `;
 
