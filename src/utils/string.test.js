fd316c115028b63d55154e11392795ea1f90f098
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 19:43:28 -0300

    Update string utils tests

--- src/utils/string.test.js
+++ src/utils/string.test.js
@@ -2,9 +2,9 @@ import { formatMoney } from './string';
 
 const mockValue = 100.15;
 
-const mockValueFormatted = "R$\xa0100.15";
+const mockValueFormatted = 'R$\xa0100.15';
 
-describe('obj', () => {
+describe('string', () => {
   it('should returns value in monetary format', () => {
     expect(formatMoney(mockValue)).toEqual(mockValueFormatted);
   });
