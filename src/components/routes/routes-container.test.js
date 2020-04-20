d72a5373087dd324a0d088fb7b182cfb013e7276
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 16:39:34 -0300

    Add RoutesContainer component tests

---
+++ src/components/routes/routes-container.test.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { MemoryRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import { mockReduxState } from '../../redux/store.mock';
+import RoutesContainer from './routes-container';
+import InvestmentsPage from '../../pages/investments';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const reduxStore = mockStore(mockReduxState);
+
+describe('RoutesContainer', () => {
+  it('should renders InvestmentsPage component', () => {
+    const wrapper = mount(
+      <Provider store={reduxStore}>
+        <MemoryRouter initialEntries={[ '/' ]}>
+          <RoutesContainer />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find(InvestmentsPage).exists()).toBeTruthy();
+  });
+});
