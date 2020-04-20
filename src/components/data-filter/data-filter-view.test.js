684a00baa0a417ef6ae5c72f1a9c80c04a2577b6
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 15:12:58 -0300

    Update DataFilterView component tests

--- src/components/data-filter/data-filter-view.test.js
+++ src/components/data-filter/data-filter-view.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import DataFilterView from './data-filter-view';
 import { mockOptions } from './data-filter-view.mock';
+import { Select } from './data-filter-view.styles';
+
+const mockSetPeriod = jest.fn();
 
 describe('DataFilterView', () => {
   it('renders correctly', () => {
@@ -9,11 +13,31 @@ describe('DataFilterView', () => {
       .create(
         <DataFilterView
           options={mockOptions}
-          setPeriod={() => {}}
+          setPeriod={mockSetPeriod}
         />
       )
       .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  test('should call setPeriod function', () => {
+    const wrapper = shallow(
+      <DataFilterView
+        options={mockOptions}
+        setPeriod={mockSetPeriod}
+      />,
+    );
+
+    wrapper
+      .find(Select)
+      .first()
+      .simulate('change', {
+        target: {
+          value: '1',
+        },
+      });
+
+    expect(mockSetPeriod).toBeCalledTimes(1);
   });
 });
