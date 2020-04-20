4605525f84d78f355f1ed3bcb0a3178bfb2086d2
Washingto Pires <4washingtonbr@gmail.com>
2020-04-20 15:11:29 -0300

    Add obj utils tests

---
+++ src/utils/obj.test.js
@@ -0,0 +1,23 @@
+import { objEquals } from './obj';
+
+const mockObjA = {
+  a: 'valueA',
+};
+
+const mockObjADuplicated = {
+  a: 'valueA',
+};
+
+const mockObjB = {
+  b: 'valueB',
+};
+
+describe('obj', () => {
+  it('should returns truth for equal objects', () => {
+    expect(objEquals(mockObjA, mockObjADuplicated)).toBeTruth();
+  });
+
+  it('should returns falsy for different objects', () => {
+    expect(objEquals(mockObjA, mockObjB)).toBeFalsy();
+  });
+});
